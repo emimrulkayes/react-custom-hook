67019aa2897eded3df0455ae34dc011dde6b420d
emimrulkayes <emimrulkayes@gmail.com>
2023-12-04 23:32:00 +0600

    Added useState Hook Structure.

--- src/Hooks/UseState/UseStateHook.js
+++ src/Hooks/UseState/UseStateHook.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const UseStateHook = () => {
+
+    const [count, setCount] = useState(0);
+
     return (
         <div>
-            This is use state hook. 
+            <h3>Use State Hook</h3>
+            <button onClick={()=>setCount(count+1)}>Increase Counter</button> 
+            <button> {count} </button>
+            <button onClick={()=>setCount(count-1)}>decrease Counter</button> 
         </div>
     );
 };
